9aec18b3b36475e5f0c466db6bb68e6ec7a844fa
程琤 <18817843924@163.com>
2024-07-31 15:11:49 +0800

    vite --host

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,8 @@ import IconsResolver from 'unplugin-icons/resolver'
 import Layouts from 'vite-plugin-vue-layouts'
 import { VitePWA } from 'vite-plugin-pwa'
 
+import { viteMockServe } from 'vite-plugin-mock'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -58,6 +60,10 @@ export default defineConfig({
     }),
     VitePWA({
       registerType: 'autoUpdate' // 自更新
+    }),
+    viteMockServe({
+      mockPath: 'mock',
+      enable: true
     })
   ],
   resolve: {
